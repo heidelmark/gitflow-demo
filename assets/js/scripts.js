318d687d12248b2e16eb58e030b1c0032f662338
Eric Heidelmark <eheidelmark@gracenote.com>
2015-11-27 13:35:09 -0500

    Added random number generator. [MERCKX-005]

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,9 @@
 (function () {
   var message = 'Sweet application, man.';
   console.log(message);
+
+  randomNumber = function () {
+    // Gaurenteed to be random. Result of one roll of a fair 6-sided die.
+    return 4;
+  }
 })()
